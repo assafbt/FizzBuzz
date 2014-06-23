a0041ed0fee89e1db87c1b62ab4d6ba1e448a60a
assafbt <assafbt@gmail.com>
2014-06-23 11:30:38 +0300

    init

---
+++ src/fizzbuzz.java
@@ -0,0 +1,63 @@
+/* 
+assaf biton
+FizzBuzz
+036918324
+
+*/
+
+
+public class fizzbuzz {
+
+	public String a[];
+	public fizzbuzz()
+	{
+		 a= new String [100];
+		
+	}
+	public void init()
+	{
+		
+		 for (int i = 0 ; i < a.length ; i++)
+		 {
+			 a[i] = Integer.toString(i);
+		 }
+	}
+	public void fizz()
+	{
+		
+		 for (int i = 0 ; i < a.length ; i++)
+		 {
+			 if(i%3 == 0)
+			{
+			 a[i] = "fizz";
+			}
+		 }
+		
+		
+	}
+	public void buzz()
+	{
+		
+		 for (int i = 0 ; i < a.length ; i++)
+		 {
+			 if(i%5 == 0)
+			{
+			 a[i] = "buzz";
+			}
+		 }
+		
+		
+	}
+	public void fizzandbuzz()
+	{
+		
+		 for (int i = 0 ; i < a.length ; i++)
+		 {
+			 if(i%5 == 0 && i%3 == 0)
+			{
+			 a[i] = "fizzbuzz";
+			}
+		 }
+				
+	}
+}
---
+++ src/fizzbuzztest.java
@@ -0,0 +1,120 @@
+/* 
+assaf biton
+FizzBuzz
+036918324
+
+*/
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class fizzbuzztest {
+
+	
+	fizzbuzz fizz = new fizzbuzz();
+	@Test
+	public void testinit() {
+		fizz.init();
+		for (int i = 0 ; i < 100 ; i++)
+		{	
+			if (fizz.a[i].equals(i))
+			{
+				fail("error");
+			}
+		}
+	
+
+	}
+	@Test
+	public void testfiz()
+	{
+		fizz.fizz();
+		for (int i = 0 ; i < 100 ; i++)
+		{	
+			if(i%3 == 0)
+			{
+				if (!fizz.a[i].equals("fizz"))
+				{
+					fail("errorfizz");
+				}
+			}
+		}
+		
+	}
+	@Test
+	public void testbuzz()
+	{
+		fizz.buzz();
+		for (int i = 0 ; i < 100 ; i++)
+		{	
+			if(i%5 == 0)
+			{
+				if (!fizz.a[i].equals("buzz"))
+				{
+					fail("errorbuzz");
+				}
+			}
+		}
+		
+	}
+	@Test
+	public void testfizzbuzz()
+	{
+
+		fizz.fizzandbuzz();
+		for (int i = 0 ; i < 100 ; i++)
+		{	
+			if(i%5 == 0 && i%3 == 0)
+			{
+				if (!fizz.a[i].equals("fizzbuzz"))
+				{
+					fail("errorfizzbuzz");
+				}
+			}
+		}
+		
+	}
+	@Test
+	public void testfizzandbuzz()
+	{
+		fizz.init();
+		fizz.fizz();
+		fizz.buzz();
+		fizz.fizzandbuzz();
+		for (int i = 0 ; i < 100 ; i++)
+		{
+			if(i%5 == 0 && i%3 == 0)
+			{
+				if (!fizz.a[i].equals("fizzbuzz"))
+				{
+					fail("errorfizzbuzz");
+				}
+			}
+			else if(i%5 == 0)
+			{
+				if (!fizz.a[i].equals("buzz"))
+				{
+					fail("errorbuzz");
+				}
+			}
+			else if(i%3 == 0)
+			{
+				if (!fizz.a[i].equals("fizz"))
+				{
+					fail("errorfizz");
+				}
+			}
+			else
+			{
+				if (fizz.a[i].equals(i))
+				{
+					fail("error");
+				}
+				
+			}
+		}
+	}
+	
+}
